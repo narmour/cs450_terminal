a0196f5634129546adcd96b924f67676fe4eddf7
narmour <n.armour0@gmail.com>
2017-03-15 13:50:28 -0700

    single and double redir working

--- exec_cmd.c
+++ exec_cmd.c
@@ -7,6 +7,7 @@ void runCmd(struct cmd*c){
     struct exec *e;
     struct redir *r;
     struct pipecmd *p;
+    int fd;
 
     pid_t pid;
     switch(c->id){
@@ -22,6 +23,52 @@ void runCmd(struct cmd*c){
                 puts("didnt run cmd");
             }
             break;
+        case '>':
+            r = (struct redir *)c;
+            pid = fork();
+            if(pid ==-1){
+                puts("ERROR");
+                break;
+            }
+            if(pid ==0){
+                switch(r->r1){
+                    // switch output
+                    case '>':
+                        if(close(1) ==-1)
+                            puts("couldnt close stdout");
+                        // open output file
+                        fd = open(r->fd1,O_WRONLY|O_CREAT|
+                                O_TRUNC,0777);
+                        // dup fd into stdout position
+                        dup(fd);
+                        execvp(r->cmd->cmd,r->cmd->args);
+                        break;
+                    // switch input
+                    case '<':
+                        if(close(0) ==-1)
+                            puts("couldnt close stdin");
+                        //open input file
+                        fd = open(r->fd1,O_RDONLY);
+                        //dup fd into stdin pos
+                        dup(fd);
+                        
+                        //if theres an output file, redir stdout
+                        if(r->r2 =='>'){
+                            if(close(1) ==-1)
+                                puts("couldnt close stdout");
+                            int fd2 = open(r->fd2,O_WRONLY|
+                                    O_CREAT|O_TRUNC,0777);
+                            //dup fd2 into stdout
+                            dup(fd2);
+                        }
+                        execvp(r->cmd->cmd,r->cmd->args);
+
+
+                }
+            }
+
+                        
+              
 
 
 
--- exec_cmd.h
+++ exec_cmd.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include "constants.h"
 #include "parse_cmd.h"
 #include "string.h"
